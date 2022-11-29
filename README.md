dc8e18ea1886749ed510c410cb4135cbea559b4e
Nicolas Denoël <nicolas@nicode.io>
2022-11-29 21:44:52 +0100

    ESLint/Prettier setup

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # Once_Upon_A_Site
+
+## Description
+
+## Patch notes
+
+### v0.0.1
+
+* ESLint & Prettier
+    * Install plugins
+    * Install TypeScript plugins
+    * Create configuration files
+* Create empty ReactJS application
+    * TypeScript
+    * CRA
--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React            from 'react';
+import {render, screen} from '@testing-library/react';
+
+import {App} from './App';
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+	render(<App/>);
+	const linkElement = screen.getByText(/Once Upon A Site/i);
+	expect(linkElement).toBeInTheDocument();
 });
